de80f14d49628e5d7f911872027b64b8d3cedc57
Jonthan Hutson <jhut110766@gmail.com>
2016-11-15 17:26:44 -0500

    nmbr

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,40 +1,46 @@
 puts "You pick the range."
 
-# Pick a random number between 1 and 100
+#array name.include?
 computer_number = rand(1..100)
 p computer_number
 count = 5
+guesses = []
 
 loop do
-  # Prompt you for a guess
+
   puts "What is your guess?"
   input_as_number = gets.chomp.to_i
 
-  # If your guess is less than the program's number,
-  # it should tell you that you were low and let you guess again.
+
   def wrong_answer wrong_answer_color
     print "\e[31m#{wrong_answer_color}\e[0m"
   end
-  if input_as_number < computer_number
-     wrong_answer "Your number is too low!"
+
+  if guesses.include? (input_as_number)
+    wrong_answer "Ummmm did you think we didn't notice you put that twcie? "
+
   end
 
-  # If the guess is greater than the program's number, it should tell you that you were high and let you guess again.
+guesses << input_as_number
+
+  if input_as_number < computer_number
+     wrong_answer "Your number is too low! "
+  end
+
   if input_as_number > computer_number
-      wrong_answer "Your number is too high!"
+      wrong_answer "Your number is too high! "
   end
 
 def right_answer right_answer_color
   print "\e[32m#{right_answer_color}\e[0m"
 
 end
-  # If your guess is correct, the program should tell you that you win and then quit.
+
   if input_as_number == computer_number
       right_answer "You got it!"
      break
   end
 
-  # After 5 incorrect guesses, the program should tell you that you lose.
   count = count - 1
   if count == 0
     wrong_answer "You guessed too many times"
